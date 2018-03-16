fc7f9fab44c0c58a1418175865e17ee5d03b54da
howardgkim <hgkim@sfu.ca>
2018-03-15 21:09:32 -0700

    device validation had typo

--- au5/app/models/device.rb
+++ au5/app/models/device.rb
@@ -2,5 +2,5 @@ class Device < ApplicationRecord
     validates :ip, presence: true
     validates :name, presence: true
     validates :category, presence: true
-    validates :ownerrv, presence: true
+    validates :owner, presence: true
 end
